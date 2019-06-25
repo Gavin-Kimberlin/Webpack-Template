2abe50391afd3b662bcc8b9cdea4a09e81c8039a
Student <example@example.com>
2019-06-25 11:16:02 -0700

    test has been added

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,16 @@
 const webpackConfig = require('./webpack.config.js');
-// Karma configuration
-// Generated on Tue Jun 25 2019 10:05:41 GMT-0700 (PDT)
 
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jquery-3.4.1','jasmine'],
-
-
-    // list of files / patterns to load in the browser
+    frameworks: ['jquery-3.2.1', 'jasmine'],
     files: [
       'src/*.js',
       'spec/*spec.js'
     ],
-
     webpack: webpackConfig,
-    // list of files / patterns to exclude
     exclude: [
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'src/*.js': ['webpack', 'sourcemap'],
       'spec/*spec.js': ['webpack', 'sourcemap']
@@ -40,42 +23,13 @@ module.exports = function(config) {
       'karma-jasmine-html-reporter',
       'karma-sourcemap-loader'
     ],
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'kjhtml'],
-
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity
   })
 }
